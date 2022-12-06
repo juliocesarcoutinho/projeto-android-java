2e82856971de099f435b28c4ec73c8729bb81f73
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-12-06 09:06:35 -0300

    Ajustes no Projeto

--- Aprenda-Ingles/app/src/main/java/com/cursoandroid/aprendaingles/MainActivity.java
+++ Aprenda-Ingles/app/src/main/java/com/cursoandroid/aprendaingles/MainActivity.java
@@ -1,14 +1,43 @@
 package com.cursoandroid.aprendaingles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.bichos, BichosFragment.class)
+                .add(R.string.numeros, NumerosFragment.class)
+                .add(R.string.vogais, VogaisFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.smartTabLayout);
+        viewPagerTab.setViewPager(viewPager);
+
+
     }
 }
